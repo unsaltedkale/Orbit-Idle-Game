7c07ce5fbd23be3e617a39df61a50102db6b1ec9
unsaltedkale <abuckley0278@student.pps.net>
2025-05-26 11:33:37 -0700

    5/26/2025
    orbitial mechanics are hard and stupid
    trying to make planet flinging work

--- Assets/Scripts/Planet_Behavior.cs
+++ Assets/Scripts/Planet_Behavior.cs
@@ -8,15 +8,15 @@ public class Planet_Behavior : MonoBehaviour
     public float orbitSpeed;
     public float rotationSpeed;
     public Vector3 mousePos;
-    public Vector3 firstMousePos; //h
-    public Vector3 secondMousePos; //j
-    public float clickDistance; //o
-    public float radiusVar; //r or d
-    public float xIntercept; //l1
-    public float yIntercept; //z1
-    public float stretchHorizontal; //a
-    public float stretchVertical; //b
-    public float finalDilation; //g1
+    public Vector3 h; //firstMousePos
+    public Vector3 j; //secondMousePos
+    public float o; //clickDistance
+    public float d; //radiusVar
+    public float l1; //xintercept
+    public float z1; //yintercept
+    public float a; //horizontalstretch
+    public float b; //vertical stretch
+    public float g1; //finalDilation
     public float theta; //c
     public Vector3 tangentIntersect;
     public float v1;
@@ -27,6 +27,7 @@ public class Planet_Behavior : MonoBehaviour
     public float v6;
     public LineRenderer lR;
     public TrailRenderer tR;
+    public bool startingPlanet;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -48,32 +49,53 @@ public class Planet_Behavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (startingPlanet == false && Input.GetKeyDown(KeyCode.R))
+        {
+            startingPlanet = true;
+            tR.enabled = false;
+            StartCoroutine(startPlanet());
+        }
+
+        mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        
         if (lR.enabled == true)
         {
             lR.SetPosition(1, mousePos / 152);
         }
 
-        mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        if (tR.enabled == true)
+        {
+            v1 = d * a * Mathf.Sin(theta);
 
-        /*theta += (orbitSpeed * Time.deltaTime);
+            v2 = d * b * Mathf.Cos(theta);
 
-        theta = theta % (2 * Mathf.PI);
+            v3 = ((l1 * Mathf.Sin(theta)) / g1);
 
-        v1 = radiusVar * stretchVertical * Mathf.Sin(theta);
-        v2 = radiusVar * stretchHorizontal * Mathf.Cos(theta);
+            v4 = ((d / g1) * (o / l1) * Mathf.Cos(theta));
 
-        transform.position = new Vector3(v1, v2, 0f);*/
+            v5 = (v1 + v3) / 2;
+
+            v6 = (v2 + v4) / 2;
+
+            theta += (orbitSpeed * Time.deltaTime);
+
+            theta = theta % (2 * Mathf.PI);
+
+            transform.position = new Vector3(v5, v6, 0f);
+        }
+        
     }
 
     public IEnumerator startPlanet()
     {
+        startingPlanet = true;
         //first click
 
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Mouse0) == true);
 
-        firstMousePos = mousePos / 152;
+        h = mousePos / 152;
 
-        transform.position = firstMousePos;
+        transform.position = h;
 
         yield return null;
 
@@ -86,7 +108,7 @@ public class Planet_Behavior : MonoBehaviour
 
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Mouse0) == true);
 
-        secondMousePos = mousePos / 152;
+        j = mousePos / 152;
 
         lR.enabled = false;
 
@@ -100,11 +122,37 @@ public class Planet_Behavior : MonoBehaviour
 
     public IEnumerator calculateOrbit()
     {
-        radiusVar = Mathf.Sqrt((Mathf.Pow(firstMousePos.x, 2)) + (Mathf.Pow(firstMousePos.y, 2)));
+        d = Mathf.Sqrt((Mathf.Pow(h.x, 2)) + (Mathf.Pow(h.y, 2)));
 
-        clickDistance = Mathf.Sqrt((Mathf.Pow(firstMousePos.x - secondMousePos.x, 2)) + (Mathf.Pow(firstMousePos.y - secondMousePos.y, 2)));
+        o = Mathf.Sqrt((Mathf.Pow(h.x - j.x, 2)) + (Mathf.Pow(h.y - j.y, 2)));
 
-        
+        l1 = ((-j.y) * ((j.x - h.x) / (j.y - h.y))) + j.x;
+
+        a = o / l1;
+
+        b = l1 / d;
+
+        g1 = 1;
+
+
+        theta = Vector2.Angle(Vector2.right, transform.position);
+
+        v1 = d * a * Mathf.Sin(theta);
+
+        v2 = d * b * Mathf.Cos(theta);
+
+        v3 = ((l1 * Mathf.Sin(theta)) / g1);
+
+        v4 = ((d / g1) * (o / l1) * Mathf.Cos(theta));
+
+        v5 = (v1 + v3) / 2;
+
+        v6 = (v2 + v4) / 2;
+
+        transform.position = new Vector3 (v5, v6, 0);
+
+        tR.enabled = true;
+        startingPlanet = false; 
 
 
         //go along line
